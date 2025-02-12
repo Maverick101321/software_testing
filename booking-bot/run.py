291bbd4f68e2be03b8aa69dfcb16976df9dd04f0
Varun Choudhary <92911349+Maverick101321@users.noreply.github.com>
2025-02-12 18:19:08 +0530

    Update run.py

--- booking-bot/run.py
+++ booking-bot/run.py
@@ -5,5 +5,6 @@ with Booking(teardown=False) as bot:
     #bot.change_currency(currency='INR')
     bot.select_place_to_go('New York')
     bot.select_dates(check_in_date='2025-02-14', check_out_date='2025-02-24')
-    bot.select_adults()
+    bot.select_adults(count=4)
+    bot.submit_search()
     input("Press Enter to close the browser...")
