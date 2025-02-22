a0ef7c674253917cef353297aa1517c0e7268ff5
Varun Choudhary <92911349+Maverick101321@users.noreply.github.com>
2025-02-22 19:13:57 +0530

    Update run.py

--- booking-bot/run.py
+++ booking-bot/run.py
@@ -1,5 +1,6 @@
 from booking.booking import Booking
 from booking.booking_filtration import BookingFiltration
+import time
 
 with Booking(teardown=False) as bot:
     bot.land_first_page()
@@ -9,7 +10,18 @@ with Booking(teardown=False) as bot:
     bot.select_adults(count=4)
     bot.submit_search()
 
+    time.sleep(5)
+
     filtration = BookingFiltration(bot.driver)
-    filtration.apply_star_rating(3, 4)
-    #bot.apply_filtrations()
+    #Applying star rating
+    filtration.apply_star_rating(3)
+    #Applying popular filters
+    filtration.apply_meals_filter("mealplan=9")
+    #Sorting results
+    filtration.sort_by_price_lowest_best_review()
+
+    bot.refresh()
+    bot.report_results()
+
+    #Adding this line so that the browser doesn't close immediately
     input("Press Enter to close the browser...")
